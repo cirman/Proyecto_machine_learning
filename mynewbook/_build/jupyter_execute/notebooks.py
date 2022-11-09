4525e4cc5e83e1467d6e402561af458a28b972e3
ciroplata <cdplata@uninorte.edu.co>
2022-11-08 21:38:57 -0500

    Build

--- mynewbook/_build/jupyter_execute/notebooks.py
+++ mynewbook/_build/jupyter_execute/notebooks.py
@@ -46,6 +46,7 @@
 # here we will import the libraries used for machine learning
 
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv), data manipulation as in SQL
+import numpy as np
 import matplotlib.pyplot as plt # this is used for the plot the graph 
 import seaborn as sns # used for plot interactive graph. I like it most for plot
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -107,7 +108,9 @@ plt.figure(figsize=(20,20))
 sns.heatmap(data.corr(),annot=True,fmt='.0%')
 
 
-# In[7]:
+# A partir de la matriz de correlacion se observa que hay grupos de variables que se relacionan entre sí. Por ejemplo, las variables radio, perimetro y area tienen una fuerte correlacion entre ellas.
+
+# In[5]:
 
 
 facet = sns.FacetGrid(data, hue="diagnosis",aspect=4)
@@ -120,5 +123,31 @@ facet = sns.FacetGrid(data, hue="diagnosis",aspect=4)
 facet.map(sns.kdeplot,'texture_mean',shade= True)
 facet.set(xlim=(0, data['texture_mean'].max()))
 facet.add_legend() 
-plt.xlim(10,20)
+plt.xlim(10,40)
+
+
+# Los tumores malignos, presentan un radio promedio mayor en comparación con los tumores malignos. Mientras que con respecto a la textura (desviación estándar de los valores de la escala de grises), los tumores malignos también muestran una puntuación promedio más alta, que los tumores benignos.
+
+# In[3]:
+
+
+cols = ["diagnosis", "radius_mean", "texture_mean", "perimeter_mean", "area_mean"]
+
+sns.pairplot(data[cols], hue="diagnosis")
+plt.show()
+
+
+# De acuerdo con los diagramas de densidad, se aprecia que las variables que mejor permiten diferenciar el tipo de tumor, son el perimetro, el area y el radio, ya que en la variable textura, ambos grupos exhiben un alto solapamiento.
+
+# In[4]:
+
+
+size = len(data['texture_mean'])
+
+area = np.pi * (15 * np.random.rand( size ))**2
+colors = np.random.rand( size )
+
+plt.xlabel("texture mean")
+plt.ylabel("radius mean") 
+plt.scatter(data['texture_mean'], data['radius_mean'], s=area, c=colors, alpha=0.5)
 
