05cc5dd8bff496aa5c72709822e938375eb1287a
ciroplata <cdplata@uninorte.edu.co>
2022-11-09 20:58:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 app = dash.Dash(__name__)
 
-
+data = pd.read_csv(".\data.csv",header=0)
 
 def my_density(x,m):
     data = pd.DataFrame({'x': x, 'm': m})
@@ -21,54 +21,19 @@ app.layout = html.Div([
     html.Label('Dropdown'),
     dcc.Dropdown(
         options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
+            {'label': 'Mean Radius', 'value': 'radius_mean'},
+            {'label': 'Mean Texture', 'value': 'texture_mean'},
+            {'label': 'Mean Perimeter', 'value': 'perimeter_mean'}
         ],
         value='MTL'
     ),
-
-    html.Label('Multi-Select Dropdown'),
-    dcc.Dropdown(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value=['MTL', 'SF'],
-        multi=True
-    ),
-
-    html.Label('Radio Items'),
-    dcc.RadioItems(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value='MTL'
-    ),
-
-    html.Label('Checkboxes'),
-    dcc.Checklist(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value=['MTL', 'SF']
-    ),
-
-    html.Label('Text Input'),
     dcc.Input(value='MTL', type='text'),
 
-    html.Label('Slider'),
-    dcc.Slider(
-        min=0,
-        max=9,
-        marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
-        value=5,
-    ),
+    dcc.Graph(
+        id='example-graph-2',
+        figure=my_density(MTL,"diagnosis")
+    )
+
 ], style={'columnCount': 2})
 
 if __name__ == '__main__':
