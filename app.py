99eb564641e0aee8d33acb775284877db3cbc4ba
ciroplata <cdplata@uninorte.edu.co>
2022-11-09 20:45:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,26 +6,70 @@ import pandas as pd
 
 app = dash.Dash(__name__)
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
+
+def my_density(x,m):
+    data = pd.DataFrame({'x': x, 'm': m})
+    facet = sns.FacetGrid(data, hue="m",aspect=4)
+    facet.map(sns.kdeplot,'x',shade= True)
+    facet.set(xlim=(0, data['x'].max()))
+    facet.add_legend() 
+    plt.show()
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+app.layout = html.Div([
+    html.Label('Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    ),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF'],
+        multi=True
+    ),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
+    html.Label('Radio Items'),
+    dcc.RadioItems(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    ),
+
+    html.Label('Checkboxes'),
+    dcc.Checklist(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF']
+    ),
+
+    html.Label('Text Input'),
+    dcc.Input(value='MTL', type='text'),
+
+    html.Label('Slider'),
+    dcc.Slider(
+        min=0,
+        max=9,
+        marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
+        value=5,
+    ),
+], style={'columnCount': 2})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
