5cd1f9e9facf8daa795db9b41d0582b72a3aa685
ciroplata <cdplata@uninorte.edu.co>
2022-11-14 17:10:37 -0500

    se agrego pca

--- app.py
+++ app.py
@@ -11,7 +11,39 @@ import plotly.figure_factory as ff
 
 app = dash.Dash(__name__)
 
-data = pd.read_csv(".\data.csv",header=0)
+data = pd.read_csv("C:/GitHub/jbook1021/data.csv",header=0)
+samples=data.iloc[:,2:32] # excluimos la variable de indentificación y la de diagnostico
+
+
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler() # el reescalador mixmax
+
+scaler.fit(samples)
+samples_scaled = scaler.transform(samples)
+
+# importar paquetes
+from sklearn.decomposition import PCA
+
+# creamos el modelo y ajustamos
+model = PCA()
+model.fit(samples_scaled)
+
+# crear un rango que enumere las característica del ACP
+caract = range(model.n_components_)
+
+
+pca = PCA(n_components=4)
+principalComponents = pca.fit_transform(samples_scaled)
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['PC 1', 'PC 2','PC 3','PC 4'])
+
+
+data_new=pd.concat([data[['diagnosis']],principalDf], axis = 1)
+
+data = pd.concat([data,principalDf], axis = 1)
 
 
 
@@ -22,7 +54,11 @@ app.layout = html.Div([
         options=[
             {'label': 'Mean Radius', 'value': 'radius_mean'},
             {'label': 'Mean Texture', 'value': 'texture_mean'},
-            {'label': 'Mean Perimeter', 'value': 'perimeter_mean'}
+            {'label': 'Mean Perimeter', 'value': 'perimeter_mean'},
+            {'label' : 'PCA1', 'value' : 'PC 1'},
+            {'label' : 'PCA2', 'value' : 'PC 2'},
+            {'label' : 'PCA3', 'value' : 'PC 3'},
+            {'label' : 'PCA4', 'value' : 'PC 4'}
         ],
         value='radius_mean'
     ),
